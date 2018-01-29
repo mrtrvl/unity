278325bf11c5dcb078d1063b7deb1ecff1812126
martti <mrt@mrt.ee>
2018-01-29 12:09:38 +0200

    Initial commit

---
+++ Assets/Scripts/BackroundScrolling.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackroundScrolling : MonoBehaviour
+{
+
+    public float speedFactor;
+
+    private Vector2 currentPosition;
+
+	void Start ()
+    {
+        currentPosition = transform.position;
+    }
+	
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal") / speedFactor;
+        float moveVertical = Input.GetAxis("Vertical") / speedFactor;
+
+        transform.position = currentPosition + (Vector2.left * moveHorizontal) + (Vector2.down * moveVertical);
+        currentPosition = transform.position;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public Text timeText;
+    public Text damageText;
+
+    private float damage;
+    private float damageFactor;
+    private Light diversLight;
+    private float lightRange;
+
+	void Start () 
+	{
+        damage = 0f;
+        damageFactor = 0.01f;
+        showText();
+        diversLight = GetComponent<Light>();
+        //diversLight.transform.position = new Vector3(0,0,5);
+    }
+
+	void Update ()
+	{
+        showText();
+    }
+
+    void OnCollisionStay2D (Collision2D collision)
+    {
+        if (collision.gameObject.name == "Roof")
+        {
+            damage += damageFactor;
+        }
+        if(collision.gameObject.name == "Bottom")
+        {
+            diversLight.range -= 0.1f;
+        }
+    }
+
+    void showText ()
+    {
+        timeText.text = "Time: " + Time.time.ToString();
+        damageText.text = "Damage: " + damage.ToString();
+    }
+}
