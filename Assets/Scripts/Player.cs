6b29c92012abf20c4b4cbfc14dcba462e0d4ff27
martti <mrt@mrt.ee>
2018-01-30 08:33:16 +0200

    Move movement to player, camera position realted to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,24 +6,28 @@ public class Player : MonoBehaviour {
 
     public Text timeText;
     public Text damageText;
+    public float horizontalSpeed = 1;
 
     private float damage;
     private float damageFactor;
     private Light diversLight;
     private float lightRange;
+    private Rigidbody2D ridgidbody;
 
 	void Start () 
 	{
         damage = 0f;
         damageFactor = 0.01f;
         showText();
-        diversLight = GetComponent<Light>();
-        //diversLight.transform.position = new Vector3(0,0,5);
+        ridgidbody = GetComponent<Rigidbody2D>();
     }
 
 	void Update ()
 	{
         showText();
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        ridgidbody.velocity = new Vector2(horizontalMove * horizontalSpeed, verticalMove);
     }
 
     void OnCollisionStay2D (Collision2D collision)
@@ -32,9 +36,10 @@ public class Player : MonoBehaviour {
         {
             damage += damageFactor;
         }
+
         if(collision.gameObject.name == "Bottom")
         {
-            diversLight.range -= 0.1f;
+            //diversLight.range -= 0.1f;
         }
     }
 
