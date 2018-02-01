a0f852e1398e271124b7fc6b6e2b2f6a304b6afe
mrtrvl <mrt@mrt.ee>
2018-02-01 10:02:10 +0200

    Buoyancy (#2)
    * First implementation of buoyancy
    
    * Change buoyancy implementation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,59 +11,53 @@ public class Player : MonoBehaviour {
     public float horizontalSpeed = 1;
     public float baseDepth = 20;
     public float defaultLightRange = 12;
-    public float gravityScaleFactor = 0.01f;
+    public float buoyancyFactor = 0.01f;
 
     private float damage;
     private float damageFactor;
     private Rigidbody2D ridgidbody;
-    private float depth;
+
     private GameObject diversLight;
     private bool offTheBottom = true;
-    private float diversGravity;
-    private float lastPositionY;
+
+    private float depth;
+    private float airVolume = 10;
 
     void Start () 
 	{
         damage = 0f;
         damageFactor = 0.01f;
+
         showText();
+
         ridgidbody = GetComponent<Rigidbody2D>();
+
         diversLight = GameObject.Find("DiveLamp");
         diversLight.GetComponent<Light>().range = defaultLightRange;
-        lastPositionY = ridgidbody.transform.position.y;
     }
 
 	void Update ()
 	{
-        showText();
-        float horizontalMove = Input.GetAxis("Horizontal");
-        float verticalMove = Input.GetAxis("Vertical");
-        ridgidbody.velocity = new Vector2(horizontalMove * horizontalSpeed, ridgidbody.velocity.y);
-
         depth = baseDepth - ridgidbody.position.y;
 
-        float currentPositionY = ridgidbody.transform.position.y;
-        float directionY = currentPositionY - lastPositionY;
-        lastPositionY = ridgidbody.transform.position.y;
+        float pressure = depth / 10 + 1;
+        float volumeUnderPressure = Mathf.Round(airVolume / pressure * 100) / 100f;
+        float buoyancy = volumeUnderPressure - 3.33f;
 
-        if ((verticalMove < 0) || (directionY < 0))
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+
+        ridgidbody.velocity = new Vector2(horizontalMove * horizontalSpeed, buoyancy);
+        //ridgidbody.AddForce(new Vector2(0, buoyancy));
+
+        if (verticalMove < 0)
         {
-            ridgidbody.gravityScale += gravityScaleFactor;
-            
-            if (ridgidbody.gravityScale > 1)
-            {
-                ridgidbody.gravityScale = 1;
-            }
+            airVolume -= buoyancyFactor;
         }
 
-        if ((verticalMove > 0) || (directionY > 0))
+        if (verticalMove > 0)
         {
-            ridgidbody.gravityScale -= gravityScaleFactor;
-
-            if (ridgidbody.gravityScale < -1)
-            {
-                ridgidbody.gravityScale = 1;
-            }
+            airVolume += buoyancyFactor;
         }
 
         if (offTheBottom)
@@ -71,6 +65,8 @@ public class Player : MonoBehaviour {
             float currentLightRange = diversLight.GetComponent<Light>().range;
             diversLight.GetComponent<Light>().range = Mathf.Lerp(currentLightRange, defaultLightRange, 0.2f * Time.deltaTime);
         }
+
+        showText();
     }
 
     void OnCollisionStay2D (Collision2D collision)
