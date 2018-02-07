35a7ef6b6265f69a8b32044689b2fe13b23aa0bf
martti <mrt@mrt.ee>
2018-02-07 15:50:43 +0200

    Add falling object with trigger

---
+++ Assets/Scripts/BackroundController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackroundController : MonoBehaviour {
+
+    public float parallaxFactor = 1.2f;
+
+    private Camera mainCamera;
+
+    void Start () {
+        mainCamera = Camera.main;
+    }
+	
+	void LateUpdate () {
+
+        moveBackground ();
+	}
+
+    void moveBackground ()
+    {
+        transform.position = new Vector3(mainCamera.transform.position.x / parallaxFactor, mainCamera.transform.position.y / parallaxFactor, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/TriggerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerController : MonoBehaviour {
+
+    public GameObject cube;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        cube.GetComponent<Rigidbody2D>().gravityScale = 0.2f;
+    }
+}
