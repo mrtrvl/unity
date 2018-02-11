a0e14a1267b42bcb8bb103eaa546e5a707e6598c
Martti Raavel <mrt@mrt.ee>
2018-02-11 21:56:34 +0200

    Change jellyfish rotation and add Bubblescontroller for audio

---
+++ Assets/Scripts/BubblesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubblesController : MonoBehaviour {
+
+    public ParticleSystem bubbles;
+    private AudioSource bubbleAudio;
+    private bool playAudio;
+
+    // Use this for initialization
+    void Start () {
+        bubbleAudio = GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(bubbles.particleCount);
+        if (playAudio)
+        {
+            bubbleAudio.Play();
+            playAudio = false;
+        } 
+
+        if (bubbles.particleCount < 10 && !bubbleAudio.isPlaying)
+        {
+            //playAudio = true;
+        }
+	}
+}
--- Assets/Scripts/JellyfishController.cs
+++ Assets/Scripts/JellyfishController.cs
@@ -2,30 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JellyfishController : MonoBehaviour {
+public class JellyfishController : MonoBehaviour
+{
 
-	private float movementDestinationX;
-	private float movementDestinationY;
-	private bool movementDirection;
-	private float movementDistance;
+    private float movementDestinationX;
+    private float movementDestinationY;
+    private bool movementDirection;
+    private float movementDistance;
     private Vector3 targetDirection;
     private Quaternion rotation;
+    private Vector3 oldPosition;
+    private Vector3 newPosition;
 
     // constants
     //	private float const 
 
 
     // Use this for initialization
-    void Start() 
-	{
+    void Start()
+    {
+        oldPosition = transform.position;
         SetFinalDestination();
     }
-	
-	// Update is called once per frame
-	void Update() 
-	{
+
+    // Update is called once per frame
+    void Update()
+    {
         MoveJellyFish();
-	}
+    }
 
     void MoveJellyFish()
     {
@@ -46,56 +50,57 @@ public class JellyfishController : MonoBehaviour {
         targetDirection = (new Vector3(movementDestinationX, movementDestinationY, 0) - transform.position).normalized;
     }
 
-	bool GetMovementDirection()
-	{
-		return Random.value <= 0.5 ? true : false;
-	}
+    bool GetMovementDirection()
+    {
+        return Random.value <= 0.5 ? true : false;
+    }
 
-	void SetMovementDestination()
-	{
-		// true = right side
-		if (movementDirection) 
-		{
-			movementDestinationX = transform.position.x + movementDistance;
-			
-		} 
-		else 
-		{
+    void SetMovementDestination()
+    {
+        // true = right side
+        if (movementDirection)
+        {
+            movementDestinationX = transform.position.x + movementDistance;
+
+        }
+        else
+        {
             movementDestinationX = transform.position.x - movementDistance;
-		}
+        }
         movementDestinationY = GetValidMovementDestinationY();
-        Debug.DrawLine(transform.position, 
+        Debug.DrawLine(transform.position,
         new Vector3(movementDestinationX, movementDestinationY, 0), Color.red, 10000.0f);
         //Debug.Log("X: " + movementDestinationX.ToString() + ", Y: " + movementDestinationY.ToString());
-			
-	}
 
-	float GetValidMovementDestinationY()
-	{
-		float tempYValue = transform.position.y + GetRandomNumber(-2.0f, 2.0f);
+    }
+
+    float GetValidMovementDestinationY()
+    {
+        float tempYValue = transform.position.y + GetRandomNumber(-2.0f, 2.0f);
         int a = 0;
         while (a <= 100)
-		{
-            if (CheckForObstaclesInMovementDirection(tempYValue)) {
+        {
+            if (CheckForObstaclesInMovementDirection(tempYValue))
+            {
                 //Debug.Log(a);
                 break;
             }
-				
-			tempYValue = transform.position.y + GetRandomNumber(-2.0f, 2.0f);
+
+            tempYValue = transform.position.y + GetRandomNumber(-2.0f, 2.0f);
             a++;
-		}
-		return tempYValue;
-	}
+        }
+        return tempYValue;
+    }
 
 
-	bool CheckForObstaclesInMovementDirection(float y)
-	{
+    bool CheckForObstaclesInMovementDirection(float y)
+    {
         Vector2 direction = new Vector2(movementDestinationX, y) - new Vector2(transform.position.x, transform.position.y);
 
         RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, 2, 1 << LayerMask.NameToLayer("Foreground"));
 
         return hit.collider == null ? true : false;
-	}
+    }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -104,18 +109,20 @@ public class JellyfishController : MonoBehaviour {
 
 
     float GetRandomNumber(float min, float max)
-	{
-		return Random.Range(min, max);
-	}
+    {
+        return Random.Range(min, max);
+    }
 
     void RotateJellyFish()
     {
-        if (targetDirection != new Vector3(0, 0, 0))
+        newPosition = transform.position;
+        Vector3 direction = newPosition - oldPosition;
+
+        if (direction != new Vector3(0, 0, 0))
         {
-            rotation = Quaternion.LookRotation(targetDirection);
+            rotation = Quaternion.LookRotation(direction);
             rotation *= Quaternion.Euler(0, 90, 90);
             transform.rotation = rotation;
-            Debug.Log("Rotation: " + targetDirection.ToString());
         }
     }
 }
