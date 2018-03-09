7c6a68f0638dfc530fb8378b1bfafe22df01e63b
martti <mrt@mrt.ee>
2018-03-09 22:01:52 +0200

    Add animation transition between divers move and hover

---
+++ Assets/Scripts/DiverAnimatorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiverAnimatorController : MonoBehaviour {
+    private Animator diverAnimator;
+    private bool move = false;
+
+	void Start () {
+        diverAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        diverAnimator.SetBool("move", move); 
+    }
+}
