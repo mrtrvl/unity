a08ad041787e549161668e5a9a3155a5d4e13c95
martti <mrt@mrt.ee>
2018-03-22 21:54:37 +0200

    Destroy rock pieces in random time after explosion

---
+++ Assets/Scripts/SetActiveFalseRandomTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetActiveFalseRandomTime : MonoBehaviour {
+
+    private float timeToSetActiveFalse;
+    private float startTime;
+
+    void Start () {
+        timeToSetActiveFalse = Random.Range(2, 5);
+        startTime = Time.time;
+    }
+	
+	void Update () {
+
+        float timeToCompare = Time.time - startTime;
+
+		if (timeToCompare > timeToSetActiveFalse)
+        {
+            gameObject.SetActive(false);
+        }
+	}
+}
