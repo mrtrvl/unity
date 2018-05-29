cbea56c767bb84fe5f8932f6e710d58ebc97bb89
Martti Raavel <mrt@mrt.ee>
2018-05-29 17:18:41 +0300

    Change how map is shown and change level button hover color

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,11 +11,15 @@ namespace bananaDiver.cameraController
         public GameObject backround;
         public float zoom;
         public float timeToShowMap;
+        public SpriteRenderer background;
 
-        private GameObject camera;
+        private new GameObject camera;
         private float defaultCameraZoom;
         private Vector3 centerPosition;
+        private Vector3 defaultPosition;
         private float cameraChangeSpeed = 15;
+        private float backToStart;
+        private Color32 backgroundColor;
 
         private Vector3 offset;
         private static bool showingMap = false;
@@ -25,29 +29,48 @@ namespace bananaDiver.cameraController
             camera = GameObject.Find("Main Camera");
             defaultCameraZoom = camera.GetComponent<Camera>().orthographicSize;
             offset = transform.position - player.transform.position;
+            backgroundColor = background.GetComponent<SpriteRenderer>().color;
+            backToStart = timeToShowMap + 2;
         }
 
         void LateUpdate()
         {
             if(!showingMap)
             {
-
                 transform.position = player.transform.position + new Vector3(0, 0, offset.z);
+                defaultPosition = transform.position;
             }
             else
             {
                 timeToShowMap -= Time.deltaTime;
                 if (timeToShowMap <= 0)
                 {
-                    backround.SetActive(true);
-                    camera.GetComponent<Camera>().orthographicSize = defaultCameraZoom;
-                    showingMap = false;
+                    backToStart -= Time.deltaTime;
+                    if (backToStart <= 0)
+                    {
+                        backround.SetActive(true);
+                        camera.GetComponent<Camera>().orthographicSize = defaultCameraZoom;
+
+                        showingMap = false;
+                    } else
+                    {
+                        transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, player.transform.position.y, offset.z),  Time.deltaTime * cameraChangeSpeed);
+                        background.GetComponent<SpriteRenderer>().color += new Color32(0, 0, 0, 1);
+                        if (transform.position == new Vector3(player.transform.position.x, player.transform.position.y, offset.z))
+                        {
+                            if (camera.GetComponent<Camera>().orthographicSize > defaultCameraZoom)
+                            {
+                                camera.GetComponent<Camera>().orthographicSize -= Time.deltaTime * cameraChangeSpeed;
+                            }
+                        }
+                    }
+
                 }
                 else
                 {
-                    backround.SetActive(false);
+                    //backround.SetActive(false);
                     centerPosition = backround.GetComponent<SpriteRenderer>().bounds.center;
-                    Debug.Log("wut");
+                    background.GetComponent<SpriteRenderer>().color -= new Color32(0, 0, 0, 1);
                     transform.position = Vector3.MoveTowards(transform.position, new Vector3(centerPosition.x, centerPosition.y, transform.position.z), Time.deltaTime * cameraChangeSpeed);
                     if (camera.GetComponent<Camera>().orthographicSize < zoom)
                     {
